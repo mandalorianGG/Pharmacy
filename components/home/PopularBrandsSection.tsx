3b0a58b2e8c5ccff3c94a7f85ee161e658f22996
mandalorianGG <skocny.jiri@gmail.com>
2023-08-25 15:37:01 +0200

    fix: brands style

--- components/home/PopularBrandsSection.tsx
+++ components/home/PopularBrandsSection.tsx
@@ -1,23 +1,25 @@
-import brands from '@/constants/brands'
-import Image from 'next/image'
-import React from 'react'
+import brands from "@/constants/brands";
+import Image from "next/image";
+import React from "react";
 
 const PopularBrandsSection = () => {
   return (
-    <section id='popular__brands' className='text-center'>
-      <h3 className='text-[18px] font-bold mb-[44px]'>Obľúbené značky</h3>
-      <div className='brands'>
+    <section id="popular__brands" className="text-center">
+      <h3 className="text-[18px] font-bold mb-[44px]">Obľúbené značky</h3>
+      <div className="brands">
         {brands.map((brand) => (
-          <Image
-            src={brand.src}
-            alt={brand.alt}
-            width={brand.width}
-            height={brand.height}
-          />
+          <span className="m-2">
+            <Image
+              src={brand.src}
+              alt={brand.alt}
+              width={brand.width}
+              height={brand.height}
+            />
+          </span>
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default PopularBrandsSection
+export default PopularBrandsSection;
