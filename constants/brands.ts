ea77a435e79257650afba291e6a3d9adcdb648b4
mandalorianGG <skocny.jiri@gmail.com>
2023-08-25 15:17:38 +0200

    fix: brand img height

--- constants/brands.ts
+++ constants/brands.ts
@@ -1,6 +1,6 @@
 const brands = [
   { src: '/images/pampers.webp', alt: 'pampers', width: 153, height: 80 },
-  { src: '/images/curaprox.webp', alt: 'curaprox', width: 154, height: 72 },
+  { src: '/images/curaprox.webp', alt: 'curaprox', width: 154, height: 30 },
   { src: '/images/kendamil.webp', alt: 'kendamil', width: 159, height: 80 },
   { src: '/images/philips.webp', alt: 'philips', width: 115, height: 60 },
   { src: '/images/wallmark.webp', alt: 'wallmark', width: 115, height: 60 },
